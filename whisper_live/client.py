1d2a62bb940b495a5e05198eb59392ad591d1cff
Dom Scott <36455793+domisjustanumber@users.noreply.github.com>
2024-06-12 16:00:18 -0400

    Limit number of lines in output to 4

--- whisper_live/client.py
+++ whisper_live/client.py
@@ -118,7 +118,7 @@ class Client:
             self.last_received_segment = segments[-1]["text"]
 
         # Truncate to last 3 entries for brevity.
-        text = text[-10:]
+        text = text[-4:]
         utils.clear_screen()
         utils.print_transcript(text)
 
